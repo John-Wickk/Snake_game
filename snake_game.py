1e3190083b7100c6b20298599a398c3a9969f619
John-Wick <115342859+John-Wickk@users.noreply.github.com>
2022-10-15 20:56:06 +0530

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -33,6 +33,60 @@ food.color("red")
 food.penup()
 food.goto(0,100)
 
+# function
+def go_up():
+    if head.direction !="down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction !="up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction !="right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction !="left":
+        head.direction = "right"
+
+
+def move():
+    if head.direction =='up':
+        y=head.ycor()
+        head.sety(y + 20)
+    if head.direction =='down':
+        y=head.ycor()
+        head.sety(y - 20)
+    if head.direction =='left':
+        x=head.xcor()
+        head.setx(x - 20)
+    if head.direction =='right':
+        x=head.xcor()
+        head.setx(x + 20)
+
+# keyword bindings
+wn.listen()
+wn.onkeypress(go_up, 'w')
+wn.onkeypress(go_down, 's')
+wn.onkeypress(go_left, 'a')
+wn.onkeypress(go_right, 'd')
+
+
+
+segments = []
+
+#pen
+
+pen=turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("score: 0 high score: 0", align="center", font=('courier', 25, "normal"))
+
 
 
 
